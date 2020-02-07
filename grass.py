cce408658c60f3c6e241ac17a8be439eee2a006e
cincottash <cincotta@udel.edu>
2020-02-06 21:38:01 -0500

    Added grass

---
+++ grass.py
@@ -0,0 +1,5 @@
+class Grass:
+	def __init__(self, color, pos, size):
+		self.color = color
+		self.pos = pos
+		self.size = size
--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import random
 import math
 from globals import *
 from rabbit import *
+from grass import *
 
 pygame.init()
 
 canvas = pygame.display.set_mode((canvasWidth, canvasHeight))
 
 rabbitList = []
+grassList = []
 
 def main():
 
-	populateCanvas(25)
+	populateCanvas(25, 25)
 
 	while True:
 		canvas.fill((255,255,255))
@@ -20,13 +22,17 @@ def main():
 		for rabbit in rabbitList:
 			pygame.draw.circle(canvas, rabbit.color, rabbit.pos , rabbit.size)
 
+		for grass in grassList:
+			pygame.draw.circle(canvas, grass.color, grass.pos, grass.size)
+
 		pygame.display.update()
 
 
-def populateCanvas(initialRabbitPop):
+def populateCanvas(startingRabbitPop, startingGrassPop):
 	currentRabbitPop = 0
+	currentGrassPop = 0
 
-	while(currentRabbitPop < initialRabbitPop):
+	while(currentRabbitPop < startingRabbitPop):
 		placed = 0
 		while(placed == 0):
 			#Create a random set of cords and a randium size
@@ -40,25 +46,50 @@ def populateCanvas(initialRabbitPop):
 					placed = 1
 					currentRabbitPop += 1
 			else:
-				canPLace = 1
+				canPlace = 1
 				for rabbit in rabbitList:
 					distance = math.sqrt((rabbit.pos[0]-x)**2 + (rabbit.pos[1]-y)**2)
+					#check for overlap of rabbits
 					if(distance <= rabbit.size+size):
-						print("overlap")
-						placed = 0
-						canPLace = 0
+						canPlace = 0
 						break	
 				
-				if(canPLace):
+				if(canPlace):
 					rabbitList.append(Rabbit((0,0,255), (x, y), size))
 					placed = 1
 					currentRabbitPop += 1
+
+	while(currentGrassPop < startingGrassPop):
+		placed = 0
+		while(placed == 0):
+			#Create a random set of cords and a randium size
+			x = random.randint(canvasWidth/4, 3*canvasWidth/4)
+			y = random.randint(canvasHeight/4, 3*canvasHeight/4)
+
+
+			#check for overlap of rabbits
+			canPlace = 1
+			for rabbit in rabbitList:
+				distance = math.sqrt((rabbit.pos[0]-x)**2 + (rabbit.pos[1]-y)**2)
+				if(distance <= rabbit.size+size):
+					canPlace = 0
+					break
+
+			#if no overlap with rabbits, check the grass too
+			if(canPlace):
+				for grass in grassList:
+					distance = math.sqrt((grass.pos[0]-x)**2 + (grass.pos[1]-y)**2)
+					if(distance <= grass.size+size):
+						canPlace = 0
+						break		
+			
+			if(canPlace):
+				grassList.append(Grass((0,255,0), (x, y), size))
+				placed = 1
+				currentGrassPop += 1
 				
 
 
 		
-
-
-
 if __name__== '__main__':
 	main()
