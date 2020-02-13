74d74044e9bc929c35f43346ab8d42e00850cd36
cincottash <cincotta@udel.edu>
2020-02-12 23:04:50 -0500

    Added color gradient to foxes

--- draw.py
+++ draw.py
@@ -25,4 +25,18 @@ def drawSprites():
 		pygame.draw.circle(canvas, grass.color, grass.pos, grass.size)
 
 	for fox in foxList:
-		pygame.draw.rect(canvas, BLACK, (fox.pos[0], fox.pos[1], fox.size, fox.size))
+		if(rabbit.hunger == 0.0):
+			pygame.draw.circle(canvas, BLACK, (int(rabbit.pos[0]), int(rabbit.pos[1])), rabbit.size)
+		#if just hungry, completly red
+		elif(rabbit.hunger < 50):
+			pygame.draw.rect(canvas, RED, (fox.pos[0], fox.pos[1], fox.size, fox.size))
+		#Not hungry
+		else:
+			red = int(255 - (((rabbit.hunger-50)/50)*255))
+			green = int(0)
+			blue = int(((rabbit.hunger-50)/50)*255)
+			pygame.draw.rect(canvas, (red, blue, green), (fox.pos[0], fox.pos[1], fox.size, fox.size))
+			
+
+
+		
